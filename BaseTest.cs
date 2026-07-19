293e29a7ee04ffb0311596998c8418d0b0e2385d
Max Komarov <you@example.com>
2026-07-19 18:22:31 -0400

    Verified Registration Form

--- BaseTest.cs
+++ BaseTest.cs
@@ -1,36 +1,74 @@
-using Microsoft.Playwright;
-using NUnit.Framework;
-namespace Tests;
+// using Microsoft.Playwright;
+// using NUnit.Framework;
+// namespace Tests;
 
-public class BaseTest
-{
-    protected IPlaywright Playwright=null!; //защита - Внутри текущего класса и его наследников
-    protected IBrowser Browser=null!; 
-        protected IBrowserContext Context = null!;
-        protected IPage Page = null!;
-    [SetUp]
-    public async Task SetUp()
-    {
+// public class BaseTest
+// {
+//     protected IPlaywright Playwright=null!; //защита - Внутри текущего класса и его наследников
+//     protected IBrowser Browser=null!; 
+//         protected IBrowserContext Context = null!;
+//         protected IPage Page = null!;
+//     [SetUp]
+//     public async Task SetUp()
+//     {
         
-        Playwright= await Microsoft.Playwright.Playwright.CreateAsync();
-        Browser=await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-        {
-            Headless=false
-        });
-    Context=await Browser.NewContextAsync(new BrowserNewContextOptions
-    {
-        ViewportSize=new ViewportSize
-        {
-            Width=1920, Height=1080
-        }
-    });
-    Page=await Context.NewPageAsync();
-    }
-    [TearDown]
-    public async Task TearDown()
-    {
-        await Context.CloseAsync();
-        await Browser.CloseAsync();
-        Playwright.Dispose();
-    }
+//         Playwright= await Microsoft.Playwright.Playwright.CreateAsync();
+//         Browser=await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+//         {
+//             Headless=false
+//         });
+//     Context=await Browser.NewContextAsync(new BrowserNewContextOptions
+//     {
+//         ViewportSize=new ViewportSize
+//         {
+//             Width=1920, Height=1080
+//         }
+//     });
+//     Page=await Context.NewPageAsync();
+//     }
+//     [TearDown]
+//     public async Task TearDown()
+//     {
+//         await Context.CloseAsync();
+//         await Browser.CloseAsync();
+//         Playwright.Dispose();
+//     }
+// }
+
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+ 
+namespace Tests;
+ 
+public class BaseTest : PageTest
+{
+    // protected const string BaseUrl = "https://www.premieronline.com/";
+ 
+    // public override BrowserTypeLaunchOptions BrowserTypeLaunchOptions()
+    // {
+    //     return new BrowserTypeLaunchOptions
+    //     {
+    //         Headless = false,
+    //         SlowMo = 100
+    //     };
+    // }
+ 
+    // public override BrowserNewContextOptions ContextOptions()
+    // {
+    //     return new BrowserNewContextOptions
+    //     {
+    //         BaseURL = BaseUrl,
+    //         ViewportSize = new ViewportSize
+    //         {
+    //             Width = 1920,
+    //             Height = 1080
+    //         }
+    //     };
+    // }
+ 
+    // [SetUp]
+    // public async Task SetUp()
+    // {
+    //     await Page.GotoAsync("/");
+    // }
 }
--- Tests/HomeTest.cs
+++ Tests/HomeTest.cs
@@ -1,4 +1,7 @@
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
 using NUnit.Framework;
+
  
 namespace Tests.Tests;
  
@@ -43,9 +46,62 @@ public class HomeTests : BaseTest
         Assert.That(await ratings.IsEnabledAsync(), Is.True);
         Assert.That(await ratings.IsVisibleAsync(), Is.True);
 
-        var help = Page.Locator("a[href=\"https://www.premieronline.com/help\"]").First;
+        //var help = Page.Locator("a[href=\"https://www.premieronline.com/help\"]").First;
         //var help = Page.Locator("nav a").Nth(2);
-        Assert.That(await help.IsEnabledAsync(), Is.True);
-        Assert.That(await help.IsVisibleAsync(), Is.True);
+        //Assert.That(await help.IsEnabledAsync(), Is.True);
+        //Assert.That(await help.IsVisibleAsync(), Is.True);
+        var help = Page.GetByRole(AriaRole.Link, new() { Name = "Help" });
+        await Expect(help).ToBeVisibleAsync();
+        await Expect(help).ToBeEnabledAsync();
+        
+    }
+
+    [Test]
+    public async Task CreateNewAccount()
+    {
+        await Page.GotoAsync("https://www.premieronline.com/");
+
+        var createAccount = Page.Locator("a:has-text('Create Account')").First;
+        await Expect(createAccount).ToBeVisibleAsync();
+        await Expect(createAccount).ToBeEnabledAsync();
+
+        await createAccount.ClickAsync();
+        await Expect(Page).ToHaveURLAsync("https://www.premieronline.com/action/register");
+
+        var emailInput = Page.Locator("#email");
+        await Expect(emailInput).ToBeVisibleAsync();
+        await Expect(emailInput).ToBeEnabledAsync();
+        await Expect(emailInput).ToHaveAttributeAsync("placeholder", "Email: we send your confirmations and receipts here.");
+        await emailInput.FillAsync("test@example.com");
+
+        var firstName = Page.Locator("#first_name");
+        await Expect(firstName).ToBeVisibleAsync();
+        await Expect(firstName).ToBeEnabledAsync();
+        await Expect(firstName).ToHaveAttributeAsync("placeholder", "First Name");
+        await firstName.FillAsync("Max");
+
+        var lastName = Page.Locator("#last_name");
+        await Expect(lastName).ToBeVisibleAsync();
+        await Expect(lastName).ToBeEnabledAsync();
+        await Expect(lastName).ToHaveAttributeAsync("placeholder", "Last Name");
+        await lastName.FillAsync("Test");
+
+        var password = Page.Locator("#password");
+        await Expect(password).ToBeVisibleAsync();
+        await Expect(password).ToBeEnabledAsync();
+        await Expect(password).ToHaveAttributeAsync("placeholder", "Password (8 characters alphanumeric)");
+        await password.FillAsync("Test123!");
+
+        var passwordRepeat = Page.Locator("#password_repeat");
+        await Expect(passwordRepeat).ToBeVisibleAsync();
+        await Expect(passwordRepeat).ToBeEnabledAsync();
+        await Expect(passwordRepeat).ToHaveAttributeAsync("placeholder", "Repeat Password");
+        await passwordRepeat.FillAsync("Test123!");
+
+        var continueButton = Page.Locator("button[type=\"submit\"]");
+        await Expect(continueButton).ToBeVisibleAsync();
+        await Expect(continueButton).ToBeEnabledAsync();
+        await continueButton.ClickAsync();
+        await Expect(Page).ToHaveURLAsync("https://www.premieronline.com/create_profile.php");
     }
 }
